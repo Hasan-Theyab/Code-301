68aa705f570ee4c2a2e1a9da82b966ace0fdfa1e
Hasan-Theyab <hhdoop70@gmail.com>
2021-08-30 15:24:16 +0300

    finished Lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <>
+      <footer>&copy; Hasan Theyab</footer>
+    
+      </>
+    )
+  }
+}
+
+export default Footer;
